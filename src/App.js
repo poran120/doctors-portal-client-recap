be7eeeaf297c2b371638286550ebe058df4a1b01
Jaker Hossain <mrporanyt120@gmail.com>
2023-10-05 20:24:35 +0600

    banner, home and navbar components are updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 // import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Navbar from './Pages/Shared/Navbar';
+import Home from './Pages/Home/Home';
+import About from './Pages/About/About';
+import Login from './Pages/Login/Login';
 
 function App() {
   return (
     <div>
-      <button className="btn accent-button text-white">Neutral</button>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='about' element={<About></About>}></Route>
+        <Route path='login' element={<Login></Login>}></Route>
+      </Routes>
     </div>
   );
 }
