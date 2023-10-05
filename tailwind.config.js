43485842c8daf8813fec587e5e80a7b39e9cfcae
Jaker Hossain <mrporanyt120@gmail.com>
2023-10-05 18:58:31 +0600

    basic setup done

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,5 +5,20 @@ module.exports = {
     extend: {},
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        doctorsPortalTheme: {
+          "primary": "#a2d2ff",
+          "secondary-card": "#caf0f8",
+          "accent-button": "#003049",
+          "neutral": "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
 }
 
